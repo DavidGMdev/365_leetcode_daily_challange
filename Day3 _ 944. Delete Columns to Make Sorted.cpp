d00b822af432691b07032e92e5fad2db9057c7f2
DavidGeorge <75450076+DavidGMdev@users.noreply.github.com>
2023-01-04 18:06:57 +0200

    Create Day3 _ 944. Delete Columns to Make Sorted.cpp

---
+++ Day3 _ 944. Delete Columns to Make Sorted.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int cnt = 0 ; 
+        for( int j = 0 ; j < strs[0].size() ; j++ ){
+            for( int i = 1 ; i < strs.size() ; i++ ) {
+                if( strs[i][j] - 'a' >= strs[i-1][j] - 'a' ) continue ;
+                cnt++ ; break ;
+            }
+        }
+        return cnt ;
+    }
+};
