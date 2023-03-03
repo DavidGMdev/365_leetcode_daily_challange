96067db7fc3daa8e51df67f91a99b66407930ca6
DavidGeorge <75450076+DavidGMdev@users.noreply.github.com>
2023-03-04 01:31:38 +0200

    Create Day_62_28. Find the Index of the First Occurrence in a String.cpp

---
+++ Day_62_28. Find the Index of the First Occurrence in a String.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        // easy solution using c++ string .find() built in function 
+        return haystack.find(needle);
+    }
+};
