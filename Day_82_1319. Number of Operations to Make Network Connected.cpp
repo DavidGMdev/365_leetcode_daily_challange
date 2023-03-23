b3f6b81df590ea4043eeea5ac413704e267e056a
DavidGeorge <75450076+DavidGMdev@users.noreply.github.com>
2023-03-23 18:41:30 +0200

    Update Day_82_1319. Number of Operations to Make Network Connected.cpp

--- Day_82_1319. Number of Operations to Make Network Connected.cpp
+++ Day_82_1319. Number of Operations to Make Network Connected.cpp
@@ -1,3 +1,36 @@
+// ##########################################
+// solution 1.1 : More optimized solution 
+// ##########################################
+
+class Solution {
+public:
+    int get( int u , vector<int>& parent ){
+        if( parent[u] == u ) return u ;
+        return parent[u] = get( parent[u] , parent );
+    }
+    void join( int u , int v , vector<int>& parent ){
+        u = get( u , parent ) , v = get( v , parent );
+        if( u != v ) parent[v] = u ; 
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if ( connections.size() < n - 1 ) return -1;
+        vector<int>parent(n);
+        for( int i = 0 ; i < n ; i++ ) parent[i] = i ; // init  
+        int links = n - 1 ;
+        for( auto & i : connections ) {
+            if ( get(i[0],parent) != get(i[1],parent) ) {
+                join(i[0],i[1],parent);
+                links--;
+            }
+        }
+        return links; // rest of links is the qunatity needed 
+    }
+};
+
+// ####################################################
+// solution 1 : using DSU and unique components check
+// ####################################################
+
 class Solution {
 public:
     int get( int u , vector<int>& parent ){
