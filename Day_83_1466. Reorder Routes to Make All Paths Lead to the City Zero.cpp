a353e240557788e5323a103c518ef27e8670625b
DavidGeorge <75450076+DavidGMdev@users.noreply.github.com>
2023-03-24 13:25:13 +0200

    Update Day_83_1466. Reorder Routes to Make All Paths Lead to the City Zero.cpp

--- Day_83_1466. Reorder Routes to Make All Paths Lead to the City Zero.cpp
+++ Day_83_1466. Reorder Routes to Make All Paths Lead to the City Zero.cpp
@@ -1,3 +1,38 @@
+// ################################
+// Solution 2 : optimization
+// ################################
+
+class Solution {
+public:
+    // more optmized version 
+    // remove useless visited , since roads are premised to be tree structure 
+    // remove inv and use pair insted in adj 
+    // if 1 then it's actually happens to be forward , we must invert it 
+    
+    void dfs( int curr , int parent , vector<vector<pair<int,int>>> & adj , int &ans ){
+        for( auto & edge : adj[curr] ){
+            int child = edge.first  , state = edge.second ; 
+            if( child == parent ) continue ; 
+            ans += state ;
+            dfs(child,curr,adj,ans);
+        }
+    }
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<pair<int,int>>> adj(n) ;
+        for( auto & i : connections ) {
+            adj[i[0]].push_back({i[1],1});
+            adj[i[1]].push_back({i[0],0});
+        }
+        int ans = 0 ; 
+        dfs(0,-1,adj,ans);
+        return ans; 
+    }
+};
+
+// ################################
+// Solution 1 : main idea
+// ################################
+
 class Solution {
 public:
     int tot = 0 ; 
