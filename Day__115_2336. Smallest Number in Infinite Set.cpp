b8e4d98cf1d191f9170d45cb63876ac38b3d884a
DavidGeorge <75450076+DavidGMdev@users.noreply.github.com>
2023-04-25 10:34:58 +0200

    Create Day__115_2336. Smallest Number in Infinite Set.cpp

---
+++ Day__115_2336. Smallest Number in Infinite Set.cpp
@@ -0,0 +1,30 @@
+class SmallestInfiniteSet {
+public:
+    priority_queue<int, vector<int>, greater<int>>q;
+    bool q_vis[1001] = {0};
+    int curr = 1; 
+    SmallestInfiniteSet() {}
+    int popSmallest() {
+        if( q.empty() || curr < q.top() ){
+            curr++;
+            return curr-1;
+        }
+        else{
+            int trg = q.top(); q.pop();
+            q_vis[trg] = false;
+            return trg;
+        }
+    }
+    void addBack(int num) {
+        if ( num >= curr || q_vis[num] ) return;
+        q_vis[num] = true;
+        q.push(num);
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
