53d0ab1b2b75759b4ec672f62a5f77d64ea3aa17
David George <75450076+DavidGMdev@users.noreply.github.com>
2023-05-09 13:24:40 +0300

    Update Day__129_54. Spiral Matrix.cpp

--- Day__129_54. Spiral Matrix.cpp
+++ Day__129_54. Spiral Matrix.cpp
@@ -38,3 +38,34 @@ for( int i = 0 ; i < ans.size() ; i++ ){
 cout << endl ;
 break;
 */
+
+// More compact version
+
+class Solution {
+public:
+    int dx[4] = {0,1,0,-1};
+    int dy[4] = {1,0,-1,0};
+    const int END = -101;
+    bool valid( int i , int j , int n , int m , vector<vector<int>>& matrix , const int END ){
+        return ( i > -1 && j > -1 && i < n && j < m && matrix[i][j] != END );
+    }
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int n = matrix.size() , m = matrix[0].size();
+        int i = 0 , j = 0 , iter = 0 ;
+        ans.push_back(matrix[i][j]);
+        matrix[i][j] = END;
+        while(ans.size() != n*m){
+            while( valid( i + dx[iter] , j + dy[iter] , n , m , matrix , END ) ) {
+                i += dx[iter] , j += dy[iter];
+                ans.push_back(matrix[i][j]);
+                matrix[i][j] = END;
+            }
+            iter++;
+            iter%=4;
+        }
+        
+        return ans;
+    }
+};
+
